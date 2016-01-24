526a756ceead6fc45eb9366f3af9c288a87f26e6
Dan Heintzelman <heintzelman@gmail.com>
2016-01-24 14:10:46 -0600

    Commit 4.6.5 Solution

---
+++ week-4/concatenate-arrays/my_solution.rb
@@ -0,0 +1,21 @@
+# Concatenate Two Arrays
+
+# I worked on this challenge [by myself].
+
+
+# Your Solution Below
+
+def array_concat(array_1, array_2)
+  combined_array = array_1 + array_2
+  return combined_array
+end
+
+# array_concat(['dog', 'biscuit', 3], [45, 'poop', 'treat'])
+
+
+#Refactor - I will try to do this in a different way and I will use a ruby method
+
+# def array_concat(array_1, array_2)
+#   array_2 = array_1.concat(array_2)
+#   return array_2
+# end
