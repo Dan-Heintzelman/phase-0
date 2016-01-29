2f352462ab6e75531467a96322c8a691895ec7a8
Dan Heintzelman <heintzelman@gmail.com>
2016-01-29 14:11:54 -0600

    Comment out reflection

--- week-5/die-class/my_solution.rb
+++ week-5/die-class/my_solution.rb
@@ -71,38 +71,38 @@ end
 
 # 4. Reflection
 
-1. What is an ArgumentError and why would you use one?
+# 1. What is an ArgumentError and why would you use one?
 
-An arguement error is an error that will display when you try to run a method that expects
-arguments and does not receive arguments that meet the initial requirements. You would use this error
-to prevent a user from being able to use your code improperly.
+# An arguement error is an error that will display when you try to run a method that expects
+# arguments and does not receive arguments that meet the initial requirements. You would use this error
+# to prevent a user from being able to use your code improperly.
 
-2.What new Ruby methods did you implement? What challenges and successes did you have in implementing them?
+# 2.What new Ruby methods did you implement? What challenges and successes did you have in implementing them?
 
-For the first time, I used the method .rand to return a random value between a specified range. The
-original way I tried to implement it worked. I used rand() and supplied a range as the arguement.
+# For the first time, I used the method .rand to return a random value between a specified range. The
+# original way I tried to implement it worked. I used rand() and supplied a range as the arguement.
 
-3.What is a Ruby class?
+# 3.What is a Ruby class?
 
-A ruby class is difficult to explain. It is like the shell of a ruby object that defines
-what that object "IS" and what it can do. It gives it the ability to have behavior beyond its
-inherent abilites as an object.
+# A ruby class is difficult to explain. It is like the shell of a ruby object that defines
+# what that object "IS" and what it can do. It gives it the ability to have behavior beyond its
+# inherent abilites as an object.
 
-4.Why would you use a Ruby class?
+# 4.Why would you use a Ruby class?
 
-Creating a ruby class allows you to give objects the inherent ability to perform certain tasks. As
-described in "The Well Grounded Rubyist", if you want an object to calculate the value of a ticket,
-you don't have to define a new ticket calculating method, you can great an object with the class of
-ticket that does this for you.
+# Creating a ruby class allows you to give objects the inherent ability to perform certain tasks. As
+# described in "The Well Grounded Rubyist", if you want an object to calculate the value of a ticket,
+# you don't have to define a new ticket calculating method, you can great an object with the class of
+# ticket that does this for you.
 
-5.What is the difference between a local variable and an instance variable?
+# 5.What is the difference between a local variable and an instance variable?
 
-An instance variable is a variable that is defined within a class that is accessibly by
-all instances of that class. A local variable is variable that is acessible only within
-its scope, such as within a method.
+# An instance variable is a variable that is defined within a class that is accessibly by
+# all instances of that class. A local variable is variable that is acessible only within
+# its scope, such as within a method.
 
-6.Where can an instance variable be used?
+# 6.Where can an instance variable be used?
 
-An instance variable can be used anywhere within the class definition. As the "Well Grounded
-Rubyist" says, "An instance variable initialized in one method insdie a clas can be used by
-any method defined within that class"
+# An instance variable can be used anywhere within the class definition. As the "Well Grounded
+# Rubyist" says, "An instance variable initialized in one method insdie a clas can be used by
+# any method defined within that class"
