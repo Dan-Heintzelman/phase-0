96cc215a4197328032cf8f9b0bd1f54388b4d4e7
Dan Heintzelman <heintzelman@gmail.com>
2016-02-03 12:59:20 -0600

    Commit 6.3 final solution

--- week-6/guessing-game/my_solution.rb
+++ week-6/guessing-game/my_solution.rb
@@ -81,20 +81,20 @@ class GuessingGame
     @guess = guess
     @solved = false
     if @answer > guess
-      return :low
+      :low
     elsif @answer < guess
-      return :high
+      :high
     else
       @solved = true
-      return :correct
+      :correct
     end
   end
 
   def solved?
     if @guess == nil
-      return false
+      false
     else
-    return @solved
+      @solved
     end
   end
 
@@ -113,35 +113,42 @@ end
 
 # Reflection
 
-1. How do instance variables and methods represent the characteristics and behaviors (actions) of
-a real-world object?
+# 1. How do instance variables and methods represent the characteristics and behaviors (actions) of
+# a real-world object?
 
-  Intance variables are a very good in helping to simulate real worl objects because they represent
-  the inherent property of an object. In the real world, an object may have a value that changes
-  based on the behavior of its actions, or what I would call a method. For instance, a dice has the
-  value of whatever side is facing up. When you roll it, it still has this property inherently,
-  but the action of rolling it, changes that value. All of the actions and values of a dice are
-  encapsulated within that particular instance of a dice. This is why the class structure makes
-  so much sense when compared to real world objects.
+#   Intance variables are a very good in helping to simulate real worl objects because they represent
+#   the inherent property of an object. In the real world, an object may have a value that changes
+#   based on the behavior of its actions, or what I would call a method. For instance, a dice has the
+#   value of whatever side is facing up. When you roll it, it still has this property inherently,
+#   but the action of rolling it, changes that value. All of the actions and values of a dice are
+#   encapsulated within that particular instance of a dice. This is why the class structure makes
+#   so much sense when compared to real world objects.
 
-2. When should you use instance variables? What do they do for you?
+# 2. When should you use instance variables? What do they do for you?
 
-  Intsance variables are most important when more than one different behavior(method) of an object
-  requires the use of that variable. Especially if more than one behavior may modify that variable.
-  For intance, if I am driving a car and pusing down the pedal, car.skinny-pedal increases the
-  speed to a value of 50 mph, and then another action car.wheel-turn (turning the wheel) reduces,
-  the speed of that car my 2mph, it is important that speed is able to be modified by both actions,
-  therefore and instance variable would be approriate.
+#   Intsance variables are most important when more than one different behavior(method) of an object
+#   requires the use of that variable. Especially if more than one behavior may modify that variable.
+#   For intance, if I am driving a car and pusing down the pedal, car.skinny-pedal increases the
+#   speed to a value of 50 mph, and then another action car.wheel-turn (turning the wheel) reduces,
+#   the speed of that car my 2mph, it is important that speed is able to be modified by both actions,
+#   therefore and instance variable would be approriate.
 
-3. Explain how to use flow control. Did you have any trouble using it in this challenge? If so,
-what did you struggle with?
+# 3. Explain how to use flow control. Did you have any trouble using it in this challenge? If so,
+# what did you struggle with?
 
-  I thought that my flow was pretty flawless at first. Then I realized that some things exist
-  ouside of the "flow". For intsance, I did not realize at first that GuessingGame.solution?
-  would need to have a starting value of false. Even though all my drive code worked, I forgot
-  to test what the answer would be if there was no guess!
+#   I thought that my flow was pretty flawless at first. Then I realized that some things exist
+#   ouside of the "flow". For intsance, I did not realize at first that GuessingGame.solution?
+#   would need to have a starting value of false. Even though all my drive code worked, I forgot
+#   to test what the answer would be if there was no guess!
 
-4. Why do you think this code requires you to return symbols? What are the benefits of using
-symbols?
+# 4. Why do you think this code requires you to return symbols? What are the benefits of using
+# symbols?
+
+#   I think this code requires us to return symbols because symbols are more efficient for Ruby to
+#   work with. Even if we were to return the string "low", this would still have to be converted to
+#   the symbol :low internally for Ruby to understand it. So this is esentially saving Ruby one
+#   step. Also, since symbols are unique, we can assume throughout the program that the object
+#   ID of :low and :high and :correct are always going to be the same, so if we run through values
+#   on objects of the guessing game class multiple times, we know that :correct is always :correct.
 
 
