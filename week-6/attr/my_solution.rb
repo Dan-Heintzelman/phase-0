2421bcbcd47cd874ec799880d1bee67f015761e0
Dan Heintzelman <heintzelman@gmail.com>
2016-02-04 16:55:05 -0600

    Commit 6.4 final solution/reflection

--- week-6/attr/my_solution.rb
+++ week-6/attr/my_solution.rb
@@ -1,14 +1,18 @@
 #Attr Methods
 
-# I worked on this challenge [by myself, with:]
+# I worked on this challenge [by myself]
 
-# I spent [#] hours on this challenge.
+# I spent [2] hours on this challenge.
 
 # Pseudocode
 
-# Input:
-# Output:
+# Input:There is no input, the instance variable @ name is set in the initialize.
+# Output: Output is a salutation to the console addressing the user @name.
 # Steps:
+# 1. Initialize the class NameData with the value of my name
+# 2. In the Greetings class, pass the value @name from the NameData class.
+# 3. In the Greetings class, defind a method hello that prints to the console the name
+# that is gathered from the NameData class.
 
 class NameData
     attr_reader :name
@@ -93,3 +97,41 @@ greet.hello
 #   The new code is 2 lines shorter and easier for ruby to understand beacuse it
 #   is using a :symbol instead. (same answer as release 2, question 3).
 
+# Final Release - Reflection
+
+# 1. What is a reader method?
+
+#   A reader method is a line of code that replaces the need to
+#   define a method to call an instance variable.
+
+# 2. What is a writer method?
+
+#   A writer method is a line of code that replaces the need to
+#   define a method to modify an instance variable. If I want to
+#   modify a value of instance variable outside of the class
+#   definition, I can do this easily with a writer method.
+
+# 3. What do the attr methods do for you?
+
+#   The attr methods make it easier to modify the attribute values
+#   of items inside of class instance and also make it more
+#   efficient to read values out of class instance.
+
+# 4. Should you always use an accessor to cover your bases? Why or
+# why not?
+
+#   An accessor is great way to both read and change the value of
+#   an insance variable without having to write two separate lines
+#   of code for both read and write. The downside to this is that
+#   it can make values in your code vulnerable by allowing them to
+#   be passed around. For instance, if you have an attr reader and
+#   writer on a credit card number, a hacker might be able to find
+#   a loophole to read this. You may only want the user to be able
+#   to modify the existing value, not read it.
+
+# 5. What is confusing to you about these methods?
+
+#   The only reason I feel that these methods are confusing is
+#   because I have not used them enough yet. I think it is easy to
+#   understand the concept of what they do, but strange to entirely
+#   remove a method from the class.
