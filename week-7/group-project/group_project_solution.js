6ae4521b7c89e717ff28981af065b5158794dd5e
Dan Heintzelman <heintzelman@gmail.com>
2016-02-11 23:30:43 -0600

    Commit 7.8 Release 1

---
+++ week-7/group-project/group_project_solution.js
@@ -0,0 +1,29 @@
+
+// Part 1 - User Story
+
+
+1. As a user, I want to have a function called sum(oddLengthArray).
+  I want it to add up the value all of the odd valued items in my list. It
+  shouldn't count any of the numbers with an even value in this list.
+
+2. I want to have a function called sum(evenLengthArray).
+  I want it to add up the value all of the even valued items in my list. It shouldn
+  't count any of the numbers with an odd value in this list.
+
+3. I want to have another function called mean. This takes the
+  starting list (oddLengthArray). This function should average the value of all
+  the items in that list that have an odd value.
+
+4. I want to have another function called mean. This takes the
+  starting list (evenLengthArray). This function should average the value of all
+  the items in that list that have an even value.
+
+5. I want to make another function called median, that
+  will give me the mean of all of the odd items in the oddLengthArray list.
+
+6. Finally, there should be another median function that gives me the same result
+  as step 5, but on the evenLengthArray list.
+
+
+// Part 2
+
